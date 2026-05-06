14dad1a63cb2d236ecd688ad19652b7f7c7de5a2
Júlia A. <juliaandrade72016@gmail.com>
2026-05-06 10:13:51 -0300

    adicionandoaparte 1
    commit barrado pelo aparelho

---
+++ riscos_petropolis/src/main/java/com/senai/backend/riscos_petropolis/RiscosPetropolisApplication.java
@@ -0,0 +1,13 @@
+package com.senai.backend.riscos_petropolis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiscosPetropolisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiscosPetropolisApplication.class, args);
+	}
+
+}
